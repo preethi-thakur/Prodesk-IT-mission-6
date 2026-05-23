7ce450fb2bb7918ddb8ba67f2fd32af1e4f5fdf0
Preethi thakur <preethithakur69@gmail.com>
2026-05-23 18:54:39 +0530

    Clean up ProductCard component formatting

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -5,7 +5,7 @@ export default function ProductCard({ product }) {
    <div className="mt-20  px-10">
       <div className="bg-white/20 backdrop-blur-lg border border-white/30 rounded-3xl overflow-hidden shadow-2xl hover:scale-105 transition duration-300 p-5">
 
-  {/* Product Image */}
+
   <div className="overflow-hidden rounded-2xl">
     <img
       src={product.thumbnail}
@@ -14,7 +14,6 @@ export default function ProductCard({ product }) {
     />
   </div>
 
-  {/* Product Details */}
   <div className="mt-5">
 
     <h2 className="text-2xl font-bold text-[#5B2E1F] truncate">
@@ -25,7 +24,7 @@ export default function ProductCard({ product }) {
       ${product.price}
     </p>
 
-    {/* Button */}
+ 
     <Link
       to={`/product/${product.id}`}
       className="mt-5 inline-block w-full text-center bg-[#FF8A65] hover:bg-[#ff7043] text-white py-3 rounded-2xl text-lg font-semibold transition duration-300 shadow-md"
